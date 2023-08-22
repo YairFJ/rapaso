c9aa78d4e27676fb388b488234d38f8c0de3bb10
Yair Farina <farinayair1@gmail.com>
2023-08-22 20:08:57 -0300

    Repaso-Java

---
+++ src/main/java/com/mycompany/repaso/Repaso.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.repaso;
+
+/**
+ *
+ * @author User
+ */
+public class Repaso {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
